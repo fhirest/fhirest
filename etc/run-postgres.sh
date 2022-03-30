915500c356f944ebd97fc7b2c12309c2b5746fe2
Daniel <daniel@kodality.com>
2022-03-30 16:11:11 +0300

    standalone pg script

--- etc/run-postgres.sh
+++ etc/run-postgres.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-CONTAINER_NAME="kefhir-postgres"
+CONTAINER_NAME="$1"
+[[ -z "$CONTAINER_NAME" ]] && CONTAINER_NAME="kefhir-postgres"
+PORT="$2"
+[[ -z "$PORT" ]] && PORT="5151"
 
 docker rm -vf $CONTAINER_NAME
 docker run -d \
  -e TZ=Europe/Tallinn \
  --restart=unless-stopped \
  --name $CONTAINER_NAME \
- -p 5151:5432 \
- docker.kodality.com/postgres-docker:14
+ -e POSTGRES_PASSWORD=postgres \
+ -p $PORT:5432 \
+ postgres:14
 
 sleep 3
-docker exec -e "DB_NAME=kefhirdb" -e "USER_PREFIX=kefhir" $CONTAINER_NAME /opt/scripts/createdb.sh
+docker exec -i $CONTAINER_NAME psql -U postgres <<-EOSQL
+CREATE ROLE kefhir_admin LOGIN PASSWORD 'test' NOSUPERUSER INHERIT NOCREATEDB CREATEROLE NOREPLICATION;
+CREATE ROLE kefhir_app   LOGIN PASSWORD 'test' NOSUPERUSER INHERIT NOCREATEDB CREATEROLE NOREPLICATION;
+CREATE DATABASE kefhirdb WITH OWNER = kefhir_admin ENCODING = 'UTF8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
+grant temp on database kefhirdb to kefhir_app;
+grant connect on database kefhirdb to kefhir_app;
+EOSQL
--- kefhir-test-app/run-tests.sh
+++ kefhir-test-app/run-tests.sh
@@ -48,9 +48,7 @@ function finish() {
 function makemeadb() {
   echo "creating database..."
   docker rm -vf $DB_DOCKER_NAME >/dev/null 2>&1
-  docker run -d -e TZ=Europe/Tallinn --restart=unless-stopped --name $DB_DOCKER_NAME -p $DB_PORT:5432 docker.kodality.com/postgres-docker:14 > test-reports/db.log || finish 1
-  sleep 6
-  docker exec -e "DB_NAME=kefhirdb" -e "USER_PREFIX=kefhir" $DB_DOCKER_NAME /opt/scripts/createdb.sh >> test-reports/db.log || finish 1
+  ../etc/run-postgres.sh $DB_DOCKER_NAME $DB_PORT || exit 1
   echo "database created."
 }
 
