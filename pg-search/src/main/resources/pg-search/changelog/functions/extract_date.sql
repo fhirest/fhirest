774e6eb27d862ed498232f1d989c37d84e3b9adf
Daniel <daniel@kodality.com>
2022-03-30 15:24:04 +0300

    standalone pg support

--- pg-search/src/main/resources/pg-search/changelog/functions/extract_date.sql
+++ pg-search/src/main/resources/pg-search/changelog/functions/extract_date.sql
@@ -3,6 +3,6 @@ RETURNS TABLE(range tstzrange)
 AS $$
   with paths as (select p.element, (p.path->>'start')::text[] lower, (p.path->>'end')::text[] upper FROM search.subpaths(_type, _path, 'date') p),
        data as (SELECT paths.*, unnest(search.jsonpath(_content, paths.element)) d from paths)
-  SELECT tstzrange('[' || coalesce(data.d#>>lower, 'infinity') || ',' || coalesce(data.d#>>upper, 'infinity') || ']')
+  SELECT tstzrange((data.d#>>lower)::timestamptz, (data.d#>>upper)::timestamptz, '[]')
   FROM data where data.d is not null
 $$ LANGUAGE SQL IMMUTABLE;
--- pg-search/src/main/resources/pg-search/changelog/functions/extract_number.sql
+++ pg-search/src/main/resources/pg-search/changelog/functions/extract_number.sql
@@ -3,6 +3,6 @@ RETURNS TABLE (range numrange)
 AS $$
   WITH paths as (select p.element, (p.path->>'lower')::text[] lower, (p.path->>'upper')::text[] upper FROM search.subpaths(_type, _path, 'number') p),
        data  as (SELECT paths.*, unnest(search.jsonpath(_content, paths.element)) d from paths)
-  SELECT numrange(coalesce((data.d#>>lower)::numeric, '-infinity') , coalesce((data.d#>>upper)::numeric, 'infinity'), '[]')
+  SELECT numrange((data.d#>>lower)::numeric, (data.d#>>upper)::numeric, '[]')
    FROM data WHERE data.d is not null
 $$ LANGUAGE SQL IMMUTABLE;
