612a55dbb2d290805fc26c26b23cf5c8bda55a4b
Daniel <daniel@kodality.com>
2023-05-30 17:26:04 +0300

    defs

--- etc/download-fhir-definitions.sh
+++ etc/download-fhir-definitions.sh
@@ -2,13 +2,14 @@
 fhir="$1"
 [[ -z "$fhir" ]] && fhir="http://localhost:8181"
 temp=/tmp/kefhir
-repo="http://hl7.org/fhir/5.0.0-draft-final/definitions.json.zip"
+repo="https://kexus.kodality.com/repository/store-public/kefhir/defs-r5.zip"
+#repo="http://hl7.org/fhir/5.0.0-draft-final/definitions.json.zip"
 #repo="https://kexus.kodality.com/repository/store-public/kefhir/definitions.json.zip"
 
 if [ ! -d $temp ]; then
   mkdir -p $temp && cd $temp
   mkdir downloads && cd downloads
-  wget $repo && unzip definitions.json.zip
+  wget $repo -O definitions.json.zip && unzip definitions.json.zip
   rm -rv definitions.json.zip fhir.schema.json.zip version.info
   mv * ..
   cd .. && rm -rf downloads
