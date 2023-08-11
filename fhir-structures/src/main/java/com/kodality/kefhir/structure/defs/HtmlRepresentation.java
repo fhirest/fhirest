3a47ed9b2352447b73046a81165728fcb5d59b35
Daniel <daniel@kodality.com>
2023-08-11 09:50:11 +0300

    html

--- fhir-structures/src/main/java/com/kodality/kefhir/structure/defs/HtmlRepresentation.java
+++ fhir-structures/src/main/java/com/kodality/kefhir/structure/defs/HtmlRepresentation.java
@@ -17,9 +17,11 @@ import java.io.IOException;
 import java.util.List;
 import javax.inject.Singleton;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.text.StringEscapeUtils;
 import org.hl7.fhir.r5.formats.IParser;
 import org.hl7.fhir.r5.formats.IParser.OutputStyle;
 import org.hl7.fhir.r5.formats.JsonParser;
+import org.hl7.fhir.r5.model.DomainResource;
 import org.hl7.fhir.r5.model.Resource;
 
 @Singleton
@@ -41,7 +43,48 @@ public class HtmlRepresentation implements ResourceRepresentation {
     try {
       IParser p = new JsonParser().setOutputStyle(OutputStyle.PRETTY);
       String json = p.composeString(resource);
-      return "<html><body><div><pre>" + json + "</pre></div></body></html>";
+      json = StringEscapeUtils.escapeHtml4(json);
+      String html = (resource instanceof DomainResource dr) ? dr.getText().getDivAsString() : null;
+      String css = """
+        body {
+          margin: 8px
+        }
+        .header {
+          margin: -8px;
+          padding: 8px;
+          font-size: 1.4em;
+          border-bottom: 1px solid;
+          padding-bottom: 13px;
+          background: #fafafa;
+        }
+        .resource-text {
+          margin-bottom: 20px;
+          border: 1px solid #ddd;
+          padding: 6px;
+          border-radius: 4px;
+        }
+        .resource-text table tbody {
+          font-size: 14px;
+        }
+        .resource-text table {
+          width: 100%;
+        }
+        .resource-json {
+          border: 1px solid #ddd;
+          border-radius: 6px;
+          background-color: #f5f5f5;
+        }
+        h1 {
+          font-size: 1.8em;
+          color: #787878;
+          margin-bottom: 0px;
+        }
+      """;
+      return "<html><head><style>"+css+"</style></head><body>"
+             + "<div class=\"header\">This is a HTML representation of a resource. <a href=\"?_format=json\">Raw json</a></div>"
+             + (html == null ? "" : "<h1>Resource narrative</h1><div class=\"resource-text\">" + html + "</div>")
+             + "<h1>Resource json</h1><div class=\"resource-json\"><pre>" + json + "</pre></div>"
+             + "</body></html>";
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
